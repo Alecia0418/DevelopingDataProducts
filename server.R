0365d4739932f77ab721cc1c5a00daf476607f9c
Alecia0418 <alecia.alianell@gmail.com>
2015-11-21 07:44:29 -0500

    1st Commit
    BMI Calculator server.R and ui.R

---
+++ server.R
@@ -0,0 +1,51 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  values <- reactiveValues()
+  
+  observe({
+    input$Calculate
+    values$bmi <- isolate({
+      (input$num_weight/(input$num_height * input$num_height))*703
+      
+    })
+  })
+
+  
+  # Display values entered
+  output$text_height <- renderText({
+    input$Calculate
+    paste("Height [in] :", isolate(input$num_height))
+  })
+  
+  output$text_weight <- renderText({
+    input$Calculate
+    paste("Weight [lb] : ", isolate(input$num_weight))
+  })
+  
+  
+  
+  # Display calculated values
+  
+  output$text_BMI <- renderText({
+    if(input$Calculate == 0) ""
+    else
+      
+      paste("BMI:", round(values$bmi,2))
+  })
+  
+  output$text_class <- renderText({
+    if(input$Calculate == 0) ""
+    else
+    if (values$bmi<18.5) info=("UNDERWEIGHT") 
+    else if (values$bmi>=8.5 & values$bmi<24.9) info=("HEALTHY WEIGHT")
+    else if (values$bmi>=25 & values$bmi<29.9) info=("OVERWEIGHT")
+    else if (values$bmi>=30) info=("OBESE")
+    else info="NA"
+
+    })
+  
+
+  
+})
---
+++ ui.R
@@ -0,0 +1,61 @@
+library(shiny)
+shinyUI(fluidPage(
+  titlePanel("BMI Calculator"),
+  titlePanel(h4("Author: Alecia Alianell")),
+  
+  sidebarPanel(
+    h3("Documentation"),
+    p(h4("Simple BMI Calculator in Standard Measurements:")),
+    br(),
+    helpText("This application calculates the BMI of a person for given height and weight. To calculate your BMI, enter your height in inches and weight in pounds into the form below."),
+    p(h3("What is BMI?")),
+    helpText("BMI stands for Body Mass Index. The BMI shows the relation between a person's height and weight, and can be used to indicate whether the person is underweight, normal weight, overweight, or obese.
+             A high BMI can be an indicator of high body fatness. BMI can be used as a screening tool but is not diagnostic of the body fatness or health of an individual. To determine if a high BMI is a health risk, a healthcare provider would need to perform further assessments. These assessments might include skinfold thickness measurements, evaluations of diet, physical activity, family history, and other appropriate health screenings"),
+    HTML("<u><b>Equation for calculation: </b></u>
+         <br> <br>
+         <b> BMI = 703*[W /(H*H)] </b>
+         <br>
+         where: <br>
+         BMI = Body Mass Index <br>
+         W = Weight in lb <br>
+         H = Height in in <br> ") 
+    
+    ),
+  
+  sidebarLayout(
+    sidebarPanel( 
+      
+      helpText("This app calculates BMI (Body Mass Index) based on your height (in) and weight (lb)."),            
+      br(),           
+      numericInput("num_height",label = h4("Enter your height (in inches)"),min = 5, max = 500,0), #,value = 150
+      br(),
+      numericInput("num_weight",label = h4("Enter your weight (in pounds)"),min = 5, max = 500,0), #, value = 50
+      br(),
+      
+      br(),   
+      actionButton("Calculate", label = "Calculate BMI")    
+      
+      
+    ),
+    mainPanel(
+      sidebarPanel(
+        "BMI Calculator",
+                 p(h4("You entered:")),
+                 textOutput("text_height"),
+                 textOutput("text_weight"),
+                 br(),
+                 p(h4("Your BMI is:")),
+                 textOutput("text_BMI"),
+                 p(h4("Based on your BMI you are considered:")),
+                 uiOutput("text_class"),
+                 br()
+                
+                 
+        
+        
+        
+        )
+      
+        )
+      )
+      ))
