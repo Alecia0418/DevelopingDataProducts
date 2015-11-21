28ea006eb152a1c557a842cdb9f02a4862cd04ed
Alecia0418 <alecia.alianell@gmail.com>
2015-11-21 07:59:12 -0500

    Final Commit
    Updates ui.R

--- ui.R
+++ ui.R
@@ -6,11 +6,13 @@ shinyUI(fluidPage(
   sidebarPanel(
     h3("Documentation"),
     p(h4("Simple BMI Calculator in Standard Measurements:")),
-    br(),
     helpText("This application calculates the BMI of a person for given height and weight. To calculate your BMI, enter your height in inches and weight in pounds into the form below."),
     p(h3("What is BMI?")),
     helpText("BMI stands for Body Mass Index. The BMI shows the relation between a person's height and weight, and can be used to indicate whether the person is underweight, normal weight, overweight, or obese.
-             A high BMI can be an indicator of high body fatness. BMI can be used as a screening tool but is not diagnostic of the body fatness or health of an individual. To determine if a high BMI is a health risk, a healthcare provider would need to perform further assessments. These assessments might include skinfold thickness measurements, evaluations of diet, physical activity, family history, and other appropriate health screenings"),
+             A high BMI can be an indicator of high body fatness. BMI can be used as a screening tool but is not diagnostic of the body fatness or health of an individual. To determine if a high BMI is a health risk, a healthcare provider would need to perform further assessments. These assessments might include skinfold thickness measurements, 
+             evaluations of diet, physical activity, family history, and other appropriate health screenings"),
+    br(),
+    helpText("Example of height in inches: If your height is 5 feet 7 inches, then you will multiply the feet by 12. Enter 5*12=60 inches + 7 inches = 67 inches"),
     HTML("<u><b>Equation for calculation: </b></u>
          <br> <br>
          <b> BMI = 703*[W /(H*H)] </b>
@@ -21,11 +23,11 @@ shinyUI(fluidPage(
          H = Height in in <br> ") 
     
     ),
-  
+  mainPanel(
   sidebarLayout(
     sidebarPanel( 
-      
-      helpText("This app calculates BMI (Body Mass Index) based on your height (in) and weight (lb)."),            
+      h3("BMI Calculator"),
+      helpText("This app calculates BMI - Body Mass Index based on your height (in) and weight (lb)."),            
       br(),           
       numericInput("num_height",label = h4("Enter your height (in inches)"),min = 5, max = 500,0), #,value = 150
       br(),
@@ -37,9 +39,9 @@ shinyUI(fluidPage(
       
       
     ),
-    mainPanel(
+   
       sidebarPanel(
-        "BMI Calculator",
+        h3("BMI Calculator Results",
                  p(h4("You entered:")),
                  textOutput("text_height"),
                  textOutput("text_weight"),
@@ -51,10 +53,10 @@ shinyUI(fluidPage(
                  br()
                 
                  
-        
-        
-        
         )
+        
+      )
+        
       
         )
       )
